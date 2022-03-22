b6a813c84ee11f75bfbaa2f3ee150418e9c617d5
adamsong <adamsong@users.noreply.github.com>
2022-03-22 00:17:40 -0500

    Addsd the last few commands

--- src/main/java/net/yogstation/yogbot/config/DiscordConfig.java
+++ src/main/java/net/yogstation/yogbot/config/DiscordConfig.java
@@ -1,5 +1,6 @@
 package net.yogstation.yogbot.config;
 
+import discord4j.common.util.Snowflake;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,11 +10,12 @@ public class DiscordConfig extends ConfigClass {
 		loadConfig("discord.properties");
 	}
 	
+	
 	public String botToken = "No token here";
-
+	
 	public String commandPrefix = "!";
 	public String serverName = "Yogstation 13";
-
+	
 	public boolean useLocalCommands = false;
 	
 	public String oauthAuthorizeUrl = "https://bab.yogstation.net/auth/authorize";
@@ -21,7 +23,7 @@ public class DiscordConfig extends ConfigClass {
 	public String oauthUserInfoUrl = "https://bab.yogstation.net/auth/userinfo";
 	public String oauthClientId = "yogstation_yogbot";
 	public String oauthClientSecret = "lolno";
-
+	
 	public long mainGuildID = 134720091576205312L;
 	public long aoRole = 471634210923216906L;
 	public long mentorRole = 505280515322937355L;
@@ -30,11 +32,12 @@ public class DiscordConfig extends ConfigClass {
 	
 	public long manualVerifyRole = 768196461313523727L;
 	public long byondVerificationRole = 762005208326733845L;
-
+	
 	// Punishment Roles
 	public long softBanRole = 302093934551629827L;
 	public long loreBanRole = 716784185192480798L;
 	public long mentorBanRole = 865043884170149928L;
+	public long wikibanRole = 792982865989992490L;
 	
 	public long firstWarningRole = 514066643119505408L;
 	public long secondWarningRole = 514066798216347662L;
